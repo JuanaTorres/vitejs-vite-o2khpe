5c6739274ae0991b12795778d05f9a88ad5bd021
Nicolas-Pena-Mogollon <nicop4643@gmail.com>
2022-06-02 23:49:40 -0500

    Corrección de errores en registro

--- src/AppRegister.tsx
+++ src/AppRegister.tsx
@@ -22,38 +22,58 @@ export default function AppLogin() {
 
     function enviarEmail() {
 
-          var url= "http://localhost:16163/MaratonProgramacion-1.0-SNAPSHOT/api/capitanes";
-          var data={
-              "id": state.id,
-              "nombre": state.nombre,
-              "idLenguaje": state.idLenguaje,
-              "email": state.email
-          }
-          fetch(url, {
-              method: 'POST',
-              body: JSON.stringify(data), // data can be `string` or {object}!
-              headers: {
-                  'Content-Type': 'application/json'
-              }
-          })
-          .then((response) => response.text())
-          .then(response => modificarMensaje(response.toString()));
-
+        var url = "http://localhost:16163/MaratonProgramacion-1.0-SNAPSHOT/api/capitanes";
+        var data = {
+            "id": state.id,
+            "nombre": state.nombre,
+            "idLenguaje": state.idLenguaje,
+            "email": state.email
+        }
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data), // data can be `string` or {object}!
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    cathError(response.text());
+                    window.location.reload();
+                }
+                return response.text()
+            })
+            .then(response => modificarMensaje(response.toString()))
+            .catch(error => cathError(error));
 
     }
 
+    function cathError(error: any) {
+        alert(error);
+        return;
+    }
+
     function modificarMensaje(m: any) {
+
+        if (m.includes("error") || m.includes("se encuentra")){
+            alert(m);
+            return;
+        }
 
         state.mensaje = "Su contraseñan de equipo es: " + m;
 
         emailjs.send("service_jnq30fj",
             "template_8v6pm8c", state, "1-3Z2otRwnMoyN3op")
             .then((response) => {
+                if (response.status !== 200){
+                    cathError(response.text);
+                    window.location.reload();
+                }
                 alert("Se a creado el integrante")
-                console.log('SUCCESS!', response.status, response.text);
+                //console.log('SUCCESS!', response.status, response.text);
             }, (err) => {
-                alert("Ocurrio un error al enviar el correo, contactate con un asesor")
-                console.log('FAILED...', err);
+                cathError(err)
+                window.location.reload();
             });
     }
 
@@ -65,7 +85,7 @@ export default function AppLogin() {
         state.email = e.target.email.value;
         state.idLenguaje = e.target.radio1.value;
         // @ts-ignore
-        state.lenguaje=document.getElementById(state.idLenguaje+"lb").textContent;
+        state.lenguaje = document.getElementById(state.idLenguaje + "lb").textContent;
         console.log(state);
         enviarEmail();
     };
@@ -88,11 +108,11 @@ export default function AppLogin() {
                         {pl: 'Nombre', name: 'name', type: 'text', icon: 'user'},
                     ].map((input) => (
                         <Input key={input.name}
-                            placeholder={input.pl} type={input.type}
-                            name={input.name} iconType={input.icon} addStyle="mb-2" required
+                               placeholder={input.pl} type={input.type}
+                               name={input.name} iconType={input.icon} addStyle="mb-2" required
                         />
                     ))}
-                    <RadioGroup key="12"list={lenguajes} required/>
+                    <RadioGroup key="12" list={lenguajes} required/>
                     <PrimaryButton key="button" type="submit" className="mb-2">Registrar</PrimaryButton>
                 </form>
                 <div className="text-sm text-black">
